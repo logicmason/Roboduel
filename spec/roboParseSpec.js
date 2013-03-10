057bb1ae73b005fc9441b71664c8ffae513be8a6
logicmason <wei.xiaoma@gmail.com>
2013-03-09 16:55:57 -0800

    adds parser for robot language and a spec

---
+++ spec/roboParseSpec.js
@@ -0,0 +1,40 @@
+var PEG = require('pegjs');
+var assert = require('chai').assert;
+var mocha = require('mocha');
+var fs = require('fs'); // for loading files
+
+var data = fs.readFileSync('../robo.peg', 'utf-8');
+// Show the PEG grammar file
+// console.log(data);
+// Create my parser
+var parse = PEG.buildParser(data).parse;
+
+assert.deepEqual(parse("fire"), ["fire"]);
+
+suite('Basic commands', function() {
+  test('move', function(){
+    assert.deepEqual(
+      parse("move"), ["move"]
+    );
+  });
+  test('left', function(){
+    assert.deepEqual(
+      parse("left"), ["left"]
+    );
+  });
+  test('right', function(){
+    assert.deepEqual(
+      parse("right"), ["right"]
+    );
+  });
+  test('fire', function(){
+    assert.deepEqual(
+      parse("fire"), ["fire"]
+    );
+  });
+  test('idle', function(){
+    assert.deepEqual(
+      parse("idle"), ["idle"]
+    );
+  });
+});
